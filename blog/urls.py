c02872a3f37c6ac843e1c8f202c5bd04ca4d98e0
karyshev_m <karyshev63rus@gmail.com>
2021-02-17 11:31:07 +0400

    progressed wfm_site project

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,7 @@ from .views import *
 urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('category/<str:slug>/', PostByCategory.as_view(), name='category'),
-    path('post/<str:slug>/', get_post, name='post'),
+    path('tag/<str:slug>/', PostsByTag.as_view(), name='tag'),
+    path('post/<str:slug>/', GetPost.as_view(), name='post'),
+    path('search/', Search.as_view(), name='search'),
 ]
